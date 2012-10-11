a57c9de492496094b1a7135bc27fdd4d78fe9832
George Lewis <ghl227@nyu.edu>
2012-10-10 21:27:38 -0400

    Removed all testing lines from the class, and they are now in the html.  Seems to still be working

--- histd3.js
+++ histd3.js
@@ -1,24 +1,15 @@
 
 // Generate an Irwin–Hall distribution of 10 random variables.
-var values = d3.range(1000).map(d3.random.irwinHall(10));
+/*
 
 
 // A formatter for counts.
-var formatCount = d3.format(",.0f");
-
-var margin = {top: 10, right: 30, bottom: 30, left: 30},
-width = 960 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
-
 var canvas_name = "#the_canvas";
 var num_bins = 20;
 
 var var_min = 0.0;
 var var_max = 2.0;
-
-
-
-// 
+*/
 
 //
 // The class
@@ -34,24 +25,37 @@ function hist(num_bins, var_min, var_max) {
 }
 
 hist.prototype.data = function(values) { 
+
     this.data_values = values; 
     return this;
+
 }
 
+hist.prototype.draw = function(div_select) {
+    
+    console.log("drawing histogram");
 
-function draw(values, num_bins, var_min, var_max, div_select) {
+    //draw(this.data_values, this.bins, this.min, this.max, div_select);
 
+    var formatCount = d3.format(",.0f");
+
+    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    //function draw(values, num_bins, var_min, var_max, div_select) {
+    
     // Create the x-axis mapping function
     var x = d3.scale.linear()
-	.domain([var_min, var_max])
+	.domain([this.min, this.max])
 	.range([0, width]);
 
     // Generate a histogram using twenty uniformly-spaced bins.
-    var data = d3.layout.histogram().bins(x.ticks(num_bins))(values);
+    var hist_bins = d3.layout.histogram().bins(x.ticks(this.bins))(this.data_values);
 
     // Create the y-axis mapping function
     var y = d3.scale.linear()
-	.domain([0, d3.max(data, function(d) { return d.y; })])
+	.domain([0, d3.max(hist_bins, function(d) { return d.y; })])
 	.range([height, 0]);
 
     // Create the x axis
@@ -59,27 +63,27 @@ function draw(values, num_bins, var_min, var_max, div_select) {
 	.scale(x)
 	.orient("bottom");
 
-    var svg = d3.select(canvas_name).append("svg")
+    var svg = d3.select(div_select).append("svg")
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var bar = svg.selectAll(".bar")
-	.data(data)
+	.data(hist_bins)
 	.enter().append("g")
 	.attr("class", "bar")
 	.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
     bar.append("rect")
 	.attr("x", 1)
-	.attr("width", x(data[0].dx) - 1)
+	.attr("width", x(hist_bins[0].dx) - 1)
 	.attr("height", function(d) { return height - y(d.y); });
 
     bar.append("text")
 	.attr("dy", ".75em")
 	.attr("y", 6)
-	.attr("x", x(data[0].dx) / 2)
+	.attr("x", x(hist_bins[0].dx) / 2)
 	.attr("text-anchor", "middle")
 	.text(function(d) { return formatCount(d.y); });
 
@@ -87,19 +91,18 @@ function draw(values, num_bins, var_min, var_max, div_select) {
 	.attr("class", "x axis")
 	.attr("transform", "translate(0," + height + ")")
 	.call(xAxis);
-}
 
-
-hist.prototype.draw = function(div_select) {
-    draw(this.data_values, this.bins, this.min, this.max, div_select);
     return this;
 }
 
-
+/*
 // Make the hist
 //hist(values, num_bins, var_min, var_max, canvas_name);
 
 var my_hist = new hist(num_bins, var_min, var_max);
 console.log( my_hist );
 
+//console.log( my_hist.data(values) );
+
 my_hist.data(values).draw(canvas_name);
+*/
